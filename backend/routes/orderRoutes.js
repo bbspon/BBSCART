2a8c9ba4749c17b92b3f15f1e80fc4d0a8ae2fa6
bbspon <bbspon@gmail.com>
2025-09-09 13:03:21 +0530

    fixed Spell mistake

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const orderController = require("../controllers/orderController");
+const orderController = require("../controllers/OrderController");
 const { authUser } = require("../middleware/authMiddleware");
 
 router.post("/", orderController.createOrder);
