6dd12cd623a1f277cc0bb6765a328473991a6933
bbspon <bbspon@gmail.com>
2025-09-09 18:02:47 +0530

    Updated api file

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL+"/api";
 const api = axios.create({
-  baseURL: API_URL,
-  timeout: 45000,
-  withCredentials: false, // set to true only if you’re using cookies
+    baseURL: "http://localhost:5000/api",
+    timeout: 45000,
+    withCredentials: false,   // set to true only if you’re using cookies
 });
 
 // Flag to track refresh attempts
