982d792e36e05e33487044b6fc6f3df52832df69
bbspon <bbspon@gmail.com>
2025-09-09 18:11:58 +0530

    update axiosInstance

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getGuestKey, ensureGuestKey } from "../utils/guestKey";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "/api",
+  baseURL: import.meta.env.VITE_API_URL || "/",
   withCredentials: true,
 });
 // If you use token-based auth, attach it here:
